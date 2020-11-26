997f67d7483978a6611498228f3a3eab1bc7108d
Ana Flávia Ferreira <40568787+AnaFlaviaFerreira@users.noreply.github.com>
2020-11-26 20:31:03 -0300

    Add files via upload

---
+++ Database/Conection.php
@@ -0,0 +1,35 @@
+<?php
+
+define('DB_HOST'        , "localhost");
+define('DB_USER'        , "sa");
+define('DB_PASSWORD'    , "123456");
+define('DB_NAME'        , "EXPRESSOAPI");
+define('DB_DRIVER'      , "sqlsrv");
+
+class Conexao
+{
+    private static $connection;
+
+    private function __construct(){}
+
+    public static function getConnection() {
+
+        $pdoConfig  = DB_DRIVER . ":". "Server=" . DB_HOST . ";";
+        $pdoConfig .= "Database=".DB_NAME.";";
+
+        try {
+            if(!isset($connection)){
+                $connection =  new PDO($pdoConfig, DB_USER, DB_PASSWORD);
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                echo "Deu certo<br>";
+            }
+            return $connection;
+         } catch (PDOException $e) {
+            $mensagem = "Drivers disponiveis: " . implode(",", PDO::getAvailableDrivers());
+            $mensagem .= "\nErro: " . $e->getMessage();
+            throw new Exception($mensagem);
+         }
+     }
+}
+
+?>
