d3cb1b0a650b6e89e2b1319860349c8ee20aca4e
Guilherme Fávaro <guifpiovan@hotmail.com>
2020-12-06 18:55:02 -0300

    back pass

--- View/FogotPassword/forgotPassword.php
+++ View/FogotPassword/forgotPassword.php
@@ -11,12 +11,13 @@ if(isset($_POST['enviar'])){
             try{
                 $from = $email;
                 $to = "";
-                $mensagem .= "Email: $email<br>";
-                $mensagem .= "Assunto: Alteração de senha. <br>";
-                $mensagem .= "Mensagem: Solicitação de alteração de senha.<br>";
+                $message.= "Email: $email<br>";
+                $message .= "Assunto: Alteração de senha. <br>";
+                $message .= "Mensagem: Solicitação de alteração de senha.<br>";
                 $headers = 'From: '.$email."\r\n". 'Reply-To: '.$email."\r\n";
                 $headers .= "MIME-Version: 1.0\r\n";
                 $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+                mail($from, $to, $message, $headers);
                }
                catch(PDOException $e){
                    echo "erro ao enviar email".$e->getMessage();
