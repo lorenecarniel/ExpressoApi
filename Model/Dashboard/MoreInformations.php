6eeb00a0515f69d8d36a66eb88cb9321f682275b
Ana Flávia A. Ferreira <ferreiraanaflaviaalves@gmail.com>
2020-12-11 00:23:14 -0300

    Update MoreInformation

--- Model/Dashboard/MoreInformations.php
+++ Model/Dashboard/MoreInformations.php
@@ -2,18 +2,37 @@
     require_once "../../Database/Conection.php";
     class MoreCallsInformation{
 
-        public static function showTableInformation($date){
+        public static function showTableInformation($date,$user){
             
             try {
                 $conectClientPlan = Connect::getConnection();
-                $sqlTableInfomationCall =$conectClientPlan->query("SELECT MONTH(CR.DTREQUEST) AS MONTH,YEAR(CR.DTREQUEST) AS YEAR,COUNT(CR.CLIENTID) AS USED,P.NAME,P.PRICE,P.REQUESTSQUANTITY,(P.REQUESTSQUANTITY -COUNT(CR.CLIENTID)) AS AVAILABLE,
-                CONVERT(DECIMAL(10,2),(P.PRICE/P.REQUESTSQUANTITY)) AS VALUECALL
-                FROM CLIENTPLAN CP, [PLAN] P, CLIENTAPIREQUEST CR
-                WHERE P.ID = CP.PLANID AND CR.CLIENTID = CP.CLIENTID AND CP.CLIENTID = 1 AND CR.URL <>'/api/sms/send' AND CR.DTREQUEST LIKE '%$date%'
-                GROUP BY P.NAME,P.PRICE,P.REQUESTSQUANTITY,MONTH(CR.DTREQUEST),YEAR(CR.DTREQUEST);");
-                $clientPlan = $sqlTableInfomationCall->fetchAll()[0];
-                    
-                return $clientPlan;
+                $restYear = substr($date, 0,4);
+                $restMonth = substr($date, 5,2);
+                try{
+                    $sqlTableInfomationCall =$conectClientPlan->query("SELECT * FROM GetMoreInformationTableInformation ('$restYear','$restMonth',$user,'$date')");
+                    $clientPlan = $sqlTableInfomationCall->fetchAll()[0];
+                    return $clientPlan;
+                }catch (Exception $e){
+                    $sqlCreateFunctionCall = $conectClientPlan->prepare("CREATE FUNCTION GetMoreInformationTableInformation(@Year nvarchar(4),@Month nvarchar(2),@User int,@full nvarchar(7))
+                    RETURNS TABLE 
+                    AS
+                    RETURN 
+                    (
+                        -- Add the SELECT statement with parameter references here
+                        SELECT @Month AS MONTH,@Year AS YEAR,ISNULL(COUNT(CR.CLIENTID),0) AS USED,
+                        IIF(COUNT(CR.CLIENTID)=0,'0',P.NAME) AS NAME,IIF(COUNT(CR.CLIENTID)=0,0,P.PRICE) AS PRICE,
+                        IIF(COUNT(CR.CLIENTID)=0,0,P.REQUESTSQUANTITY) AS REQUESTSQUANTITY,(P.REQUESTSQUANTITY - IIF(COUNT(CR.CLIENTID)=0,P.REQUESTSQUANTITY,COUNT(CR.CLIENTID))) AS AVAILABLE,
+                        IIF(COUNT(CR.CLIENTID)=0,0,CONVERT(DECIMAL(10,2),(P.PRICE /P.REQUESTSQUANTITY))) AS VALUECALL
+                        FROM CLIENTPLAN CP
+                        LEFT OUTER JOIN [PLAN] P ON P.ID = CP.PLANID
+                        LEFT OUTER JOIN CLIENTAPIREQUEST CR ON CR.CLIENTID = CP.CLIENTID AND CP.CLIENTID = @User AND CR.URL <> '/api/sms/send' AND CR.DTREQUEST LIKE '%' + @full + '%'
+                        GROUP BY P.NAME,P.PRICE,P.REQUESTSQUANTITY,MONTH(CR.DTREQUEST),YEAR(CR.DTREQUEST)
+                    )");
+                    $sqlCreateFunctionCall->execute();
+                    $sqlTableInfomation = $conectClientPlan->query("SELECT * FROM GetMoreInformationTableInformation ('$restYear','$restMonth',$user,'$date')");
+                    $result = $sqlTableInfomation->fetchAll()[0];
+                    return $result;
+                }
              } catch (Exception $e) {
                 //qual é a mensagem de erro
                 $message = "\nErro: " . $e->getMessage();
